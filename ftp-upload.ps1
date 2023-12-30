f36aeb29c715b29e163d1176fa9def556fe5f69c
LittleZiG <fgtbg@yahoo.fr>
2023-12-30 03:43:51 +0100

    Add files via upload

---
+++ ftp-upload.ps1
@@ -0,0 +1,25 @@
+function ftp-upload {
+    param (
+        [string]$ftpServer,
+        [string]$remotePath,
+        [string]$ftpUsername,
+        [string]$ftpPassword,
+        [string]$localFilePath
+    )
+
+    try {
+        $webClient = New-Object System.Net.WebClient
+        $webClient.Credentials = New-Object System.Net.NetworkCredential($ftpUsername, $ftpPassword)
+
+        $uri = New-Object System.Uri("$ftpServer$remotePath/$(Get-Item $localFilePath).Name")
+        $webClient.UploadFile($uri, $localFilePath)
+
+        Write-Host "Le fichier a été uploadé avec succès."
+    }
+    catch {
+        Write-Host "Erreur lors de l'upload du fichier : $_.Exception.Message"
+    }
+    finally {
+        $webClient.Dispose()
+    }
+}
