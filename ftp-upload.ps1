c920edbc744e8ab38a2ab117fbff082a73a4a6c1
LittleZiG <fgtbg@yahoo.fr>
2023-12-30 14:33:41 +0100

    Update ftp-upload.ps1

--- ftp-upload.ps1
+++ ftp-upload.ps1
@@ -11,7 +11,7 @@ function ftp-upload {
         $webClient = New-Object System.Net.WebClient
         $webClient.Credentials = New-Object System.Net.NetworkCredential($ftpUsername, $ftpPassword)
         
-        $webClient.UploadFile("ftp://ftp.zigflip.fun/log/a.zip", "c:\a.zip")
+        $webClient.UploadFile("ftp://ftp.zigflip.fun/log/a.zip", $localFilePath)
 
         Write-Host "Le fichier a été uploadé avec succès."
     }
