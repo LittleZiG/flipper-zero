bcc7b97c156442977699437375497aae698201ec
LittleZiG <fgtbg@yahoo.fr>
2024-01-03 00:05:04 +0100

    Add files via upload

---
+++ ftp-upload-valide.ps1
@@ -0,0 +1,24 @@
+function ftp-upload {
+    param (
+        [string]$ftpServer,
+        [string]$remotePath,
+        [string]$ftpUsername,
+        [string]$ftpPassword,
+        [string]$localFilePath
+    )
+
+    try {
+        $webClient = New-Object System.Net.WebClient
+        $webClient.Credentials = New-Object System.Net.NetworkCredential('zigfl2268087', 'M0tdep@ssezig')
+        
+        $webClient.UploadFile("ftp://ftp.zigflip.fun/log/a.zip", "c:\a.zip")
+
+        Write-Host "Le fichier a été uploadé avec succès."
+    }
+    catch {
+        Write-Host "Erreur lors de l'upload du fichier : $_.Exception.Message"
+    }
+    finally {
+        $webClient.Dispose()
+    }
+}
